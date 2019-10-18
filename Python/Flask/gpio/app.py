8ef7340fab8b6024a653dcb3999c2835ccd68e2c
bkbazemore <bbazemo33@charlottesvilleschools.org>
2019-10-18 11:21:10 -0400

    it's been real with flask...

--- Python/Flask/gpio/app.py
+++ Python/Flask/gpio/app.py
@@ -4,19 +4,46 @@ import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17,GPIO.OUT)
+GPIO.setup(27,GPIO.OUT)
+#greenstate = 0
 
 app = Flask(__name__)
 print("starting")
 @app.route("/", methods=["GET","POST"])
 def index():
+	greenstate = "off"
+	bluestate = "off"
 	print("index called")
 	if request.method == 'POST':
-		GPIO.output(17,GPIO.HIGH)
-		msg = request.form.get("submitBtn")
+		#GPIO.output(17,GPIO.HIGH)
+		#GPIO.output(27,GPIO.HIGH)
+		mylist = request.form.to_dict()
+		try:
+			var = mylist["submit1.x"]
+			print(GPIO.input(17))
+			if GPIO.input(17)  == 1:
+				GPIO.output(17, GPIO.LOW)
+				greenstate = "off"
+				
+			else:
+				GPIO.output(17, GPIO.HIGH)
+				greenstate = "on"
+		except:
+			print(GPIO.input(27))
+			if GPIO.input(27) == 1:
+				GPIO.output(27, GPIO.LOW)
+				bluestate = "off"
+			else:
+				GPIO.output(27, GPIO.HIGH)
+				bluestate = "on"
+		msg = "working?"
 	else:
 		GPIO.output(17,GPIO.LOW)
+		GPIO.output(27,GPIO.LOW)
 		msg = "no click yet"
-	return render_template("index.html", msg=msg)
+		greenstate = "off"
+		bluestate = "off"
+	return render_template("index.html", msg=msg, green=greenstate, blue=bluestate)
 
 if __name__ == "__main__":
 	print("app called")
